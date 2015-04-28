eb7c814bbed08f3b367515ecba35d449b763c997
a59478 <a226355180829@gmail.com>
2015-04-28 17:57:36 +0800

    B AM021331 吳致霆提交editFile

--- Question2.java
+++ Question2.java
@@ -3,5 +3,18 @@
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
+		public void editFile (String) {
+			FileInputStream fs;
+			int z;
+			try{
+				fs = new FileInputStream("Source.txt");	
+				while((z = fs.read()) != -1){	          
+					System.out.print((char) z);      
+				}
+				fs.close();				
+			} catch (IOException e){
+				System.out.println(e.toString());
+			}
+		}
 	}
 }
