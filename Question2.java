5441c562b2697252d791b78ae0b5ca2b3b5a49d3
AM021336 <s87033tw@gmail.com>
2015-04-28 18:32:51 +0800

    C AM021336 汪煒傑提交

--- Question2.java
+++ Question2.java
@@ -5,37 +5,54 @@ public class Question2{
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
-<<<<<<< HEAD
-		public void editFile (String) {
-			FileInputStream fs;
-			int z;
-			try{
-				fs = new FileInputStream("Source.txt");	
-				while((z = fs.read()) != -1){	          
-					System.out.print((char) z);      
-=======
+	}
 
-		public void txtCopy (String a,String b) {
-			FileInputStream fs;
-			int i;
-			try{
-				fs = new FileInputStream("Source.txt");	
-				System.out.println("檔案的大小為：" + fs.available() + " Bytes");
-				System.out.println("檔案的內容為：");
-				while((i = fs.read()) != -1){	          
-					System.out.print((char) i);      
->>>>>>> 31c0c58e15749a103e8f9c57181ca369d468c690
-				}
-				fs.close();				
-			} catch (IOException e){
-				System.out.println(e.toString());
+	public void editFile (String) {
+		FileInputStream fs;
+		int z;
+		try{
+			fs = new FileInputStream("Source.txt");	
+			while((z = fs.read()) != -1){	          
+				System.out.print((char) z);    
+	}  
+
+	public void txtCopy (String a,String b) {
+		FileInputStream fs;
+		int i;
+		try{
+			fs = new FileInputStream("Source.txt");	
+			System.out.println("檔案的大小為：" + fs.available() + " Bytes");
+			System.out.println("檔案的內容為：");
+			while((i = fs.read()) != -1){	          
+				System.out.print((char) i);      
 			}
+			fs.close();				
+		} catch (IOException e){
+			System.out.println(e.toString());
 		}
 	}
-}
-<<<<<<< HEAD
-=======
 
-
+	public static void updateFile(String file) throws IOException {
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
 	
->>>>>>> 31c0c58e15749a103e8f9c57181ca369d468c690
+		FileWriter fw = new FileWriter("a.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+	
+		String a="";
+		String temp="";
+		String[] temp2;
+	
+		while((a=br.readLine())!=null){
+			temp += a+" ";
+		}
+
+		temp2 = temp.split(" ");
+
+		for(int i=0;i<temp2.length;i++){
+				bw.write(temp2[i]);
+				bw.newLine();
+		}
+		bw.close();		
+	}
+}
