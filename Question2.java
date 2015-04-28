60705529039502303dd73ea978f0c6395bfd57b5
unknown <you@example.com>
2015-04-28 17:41:10 +0800

    A AM021711 李昱緯提交 txtCopy

--- Question2.java
+++ Question2.java
@@ -1,7 +1,28 @@
-﻿public class Question2{
+﻿import java.io.*
+
+public class Question2{
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
+
+		public void txtCopy (String a,String b) {
+			FileInputStream fs;
+			int i;
+			try{
+				fs = new FileInputStream("Source.txt");	
+				System.out.println("檔案的大小為：" + fs.available() + " Bytes");
+				System.out.println("檔案的內容為：");
+				while((i = fs.read()) != -1){	          
+					System.out.print((char) i);      
+				}
+				fs.close();				
+			} catch (IOException e){
+				System.out.println(e.toString());
+			}
+		}
 	}
 }
+
+
+	
